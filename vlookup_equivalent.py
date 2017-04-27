ce1d4ada8db0e4d86c9a43ea2e3e4e825f0c92b1
Greg Lever <greg.lever@genomicsengland.co.uk>
2017-04-27 11:28:49 +0100

    simple python version of vlookup equivalent

---
+++ vlookup_equivalent.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+def run_main():
+    df = pd.DataFrame()
+    df['columnA'] = ['disease1', 'disease2', 'disease3']
+    df['columnB'] = ['disease4', 'disease6', 'disease5']
+    df['participantIds'] = [112, 154, 182]
+    df['idsToCheck'] = [182, 112, 154]
+
+    df['columnACheck'] = [df.columnA.iloc[df[df.participantIds == idToCheck].index[0]] for idToCheck in df.idsToCheck.tolist()]
+    df['columnBCheck'] = [df.columnB.iloc[df[df.participantIds == idToCheck].index[0]] for idToCheck in df.idsToCheck.tolist()]
+
+if __name__ == '__man__':
+    run_main()
